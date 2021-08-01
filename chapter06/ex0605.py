ca9805cd3eca4d914878a78c59d718f6b89ab42b
KyawSaw <kyawsaw.mm@gmail.com>
2021-08-01 23:30:29 +0630

    edit ex0605.py

--- chapter06/ex0605.py
+++ chapter06/ex0605.py
@@ -1,7 +1,7 @@
 #print('chpater06_ex05')
 data = 'X-DSPAM-Confidence:0.8475'
 fcpos = data.find(':')
-#print(dotpos)
+#print(fcpos)
 str_number = data[fcpos+1:]
 #print(str_number)
 float_number = float(str_number)
