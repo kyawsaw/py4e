0e4c52cb4120e7d569f61da20fa5ff97157e4c54
KyawSaw <kyawsaw.mm@gmail.com>
2021-08-10 21:35:29 +0630

    some mod

--- chapter08/min_in_array.py
+++ chapter08/min_in_array.py
@@ -1,8 +1,15 @@
 list = [22, 14, 27, 55, 11]
 min = list[0]
-fount = 0
+max = list[0]
 for (i, item) in enumerate(list):
     if min > item:
         min = item
-        found = i
-print("Min value is", min, "found at", found)
+        min_found = i
+    if max < item:
+        max = item
+        max_found = i
+print("Max value is", max, "found at", max_found)
+print("Min value is", min, "found at", min_found)
+list[max_found] = min
+list[min_found] = max
+print(list)
